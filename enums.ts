18873d34342e8cc3d605f5dcd83c6a31e3850ee0
Julian Florez <forezjuli08@hotmail.com>
2023-05-17 09:54:41 -0500

    add enums

---
+++ enums.ts
@@ -0,0 +1,42 @@
+// const beforeLoad = 'beforeLoad'
+// const loading = 'loading'
+// const loaded = 'loaded'
+
+enum LoadingState {
+  //> we can get rid of the equality and the enums will start from 0 and go in an ascendent manner
+  // beforeLoad  // 0
+  // loading     // 1
+  // loaded      // 2
+  beforeLoad = 'beforeLoad',
+  loading = 'loading',
+  loaded = 'loaded',
+}
+
+const englishLoadingStates = {
+  [LoadingState.beforeLoad]: 'Before Load',
+}
+
+const isLoading = (state: LoadingState) => state === LoadingState.loading
+
+console.log(isLoading(LoadingState.beforeLoad))
+
+//* literal types
+
+function rollDice(dice: 1 | 2 | 3): number {
+  let pip = 0
+  for (let i = 0; i < dice; i++) {
+    pip += Math.floor(Math.random() * 5) + 1
+  }
+
+  return pip
+}
+
+console.log(rollDice(2))
+
+function sendEvent(name: 'addToCart', data: { productId: number }): void
+function sendEvent(name: 'checkout', data: { cartCount: number }): void
+function sendEvent(name: string, data: unknown): void {
+  console.log(`${name}: ${JSON.stringify(data)}`)
+}
+
+sendEvent('addToCart', { productId: 3213123 })
